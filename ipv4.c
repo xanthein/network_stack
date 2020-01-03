eb4154a8c2bbd8ede041b554e478f799d6874129
Dirk Su <dirksu@gmail.com>
2020-01-03 17:32:01 +0800

    Add ip receive handler

--- ipv4.c
+++ ipv4.c
@@ -1,4 +1,67 @@
+#include <stdio.h>
+#include <netinet/in.h>
+#include "netdev.h"
+#include "ethernet.h"
 #include "ipv4.h"
+
+uint16_t calculate_ipv4_checksum(uint16_t *header)
+{
+	uint32_t sum = 0;
+
+	for(int i=0;i<10;i++)
+		sum += header[i];
+
+	return ~((sum>>16) + (sum&0xffff));
+}
+
+int ipv4_read(uint8_t *buffer, uint32_t size)
+{
+	struct ether_frame *ether = (struct ether_frame *)buffer;
+	struct ipv4_header *header = (struct ipv4_header *)(ether->data);
+	uint16_t checksum = calculate_ipv4_checksum((uint16_t *)header);
+
+	if(checksum) {
+		printf("ipv4 checksum error\n");
+		free(buffer);
+		return -1;
+	}
+
+	if(header->version != 4) {
+		printf("version not match\n");
+		free(buffer);
+		return -1;
+	}
+
+	if(header->ihl < 5) {
+		printf("header length too short\n");
+		free(buffer);
+		return -1;
+	}
+
+	//TODO need to handle fragmentation
+
+	header->length = ntohs(header->length);
+	header->identification = ntohs(header->identification);
+	header->src_ip = ntohl(header->src_ip);
+	header->dst_ip = ntohl(header->dst_ip);
+
+	switch(header->protocol) {
+		case IP_ICMP:
+			printf("icmp\n");
+			break;
+		case IP_TCP:
+			printf("tcp\n");
+			break;
+		case IP_UDP:
+			printf("udp\n");
+			break;
+		default:
+			printf("ip protocol not support\n");
+	}
+
+	free(buffer);
+	return 0;
+}
 
 void ipv4_init()
 {
--- ipv4.h
+++ ipv4.h
@@ -7,8 +7,30 @@ struct ipv4_info_t {
 	uint32_t ip;
 };
 
+struct ipv4_header {
+	uint8_t ihl : 4;
+	uint8_t version : 4;
+	uint8_t ecn : 2;
+	uint8_t dscp : 6;
+	uint16_t length;
+	uint16_t identification;
+	uint16_t fragment_offset;
+	uint8_t ttl;
+	uint8_t protocol;
+	uint16_t checksum;
+	uint32_t src_ip;
+	uint32_t dst_ip;
+	uint8_t data[];
+} __attribute__((packed));
+
+#define IP_ICMP	0x01
+#define IP_TCP	0x06
+#define IP_UDP	0x11
+
 struct ipv4_info_t ipv4_info;
 
 void ipv4_init();
+int ipv4_read(uint8_t *buffer, uint32_t size);
+void ipv4_write();
 
 #endif
